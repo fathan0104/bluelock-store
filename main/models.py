e34179add913891e0abc1620aae4cb78e1062c5a
fathan0104 <fathan.alfahrezi@ui.ac.id>
2025-09-24 10:43:33 +0700

    Tugas4

--- main/models.py
+++ main/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True, null=True)
     price = models.IntegerField(blank=True, null=True, default=0)
     description = models.TextField(blank=True, null=True)
--- main/views.py
+++ main/views.py
@@ -3,11 +3,25 @@ from django.http import HttpResponse
 from django.core import serializers
 from main.models import Product
 from main.forms import ProductForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()
+    product = product.objects.filter(user=request.user)
     context = {
-        'products': products,
+        'npm': '2406496284',
+        'name': request.user.username,
+        'class' : 'PBP E',
+        'product_list': product,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
 
@@ -20,7 +34,9 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -59,3 +75,35 @@ def show_json_by_id(request, product_id):
         json_data = serializers.serialize("json", product_item)
         return HttpResponse(json_data, content_type="application/json")
     return HttpResponse(status=404)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
